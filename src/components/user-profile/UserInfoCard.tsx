8c2fae3bf3645debbd8e3a19c09d82d7af1e0762
adrianmaza <adrianmaza@gmail.com>
2025-06-25 18:08:36 -0300

    Add files via upload

--- src/components/user-profile/UserInfoCard.tsx
+++ src/components/user-profile/UserInfoCard.tsx
@@ -148,7 +148,7 @@ export default function UserInfoCard() {
                 <div className="grid grid-cols-1 gap-x-6 gap-y-5 lg:grid-cols-2">
                   <div className="col-span-2 lg:col-span-1">
                     <Label>First Name</Label>
-                    <Input type="text" defaultValue="Musharof" />
+                    <Input type="text" defaultValue="Carla" />
                   </div>
 
                   <div className="col-span-2 lg:col-span-1">
